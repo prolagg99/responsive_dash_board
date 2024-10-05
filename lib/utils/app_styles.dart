61ca7713fe4c1f2b110849fa6ffbdac269eaab6f
lagg99 <pro.lagg99@gmail.com>
2024-10-05 18:35:34 +0100

    generate assets

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,75 @@
+// abstracts cuz i do not need an object of this class
+
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const TextStyle styleRegular16 = TextStyle(
+    color: Color(0xff064061),
+    fontFamily: 'Montserrat',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleMeduim16 = TextStyle(
+    color: Color(0xff064061),
+    fontFamily: 'Montserrat',
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle styleSemiBold16 = TextStyle(
+    color: Color(0xff064061),
+    fontFamily: 'Montserrat',
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleSemiBold20 = TextStyle(
+    color: Color(0xff064061),
+    fontFamily: 'Montserrat',
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular12 = TextStyle(
+    color: Color(0xffAAAAAA),
+    fontFamily: 'Montserrat',
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleSemiBold24 = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontFamily: 'Montserrat',
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular14 = TextStyle(
+    color: Color(0xffAAAAAA),
+    fontFamily: 'Montserrat',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleSemiBold18 = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontFamily: 'Montserrat',
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleBold16 = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontFamily: 'Montserrat',
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle styleMeduim20 = TextStyle(
+    color: Color(0xffffffff),
+    fontFamily: 'Montserrat',
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+}
