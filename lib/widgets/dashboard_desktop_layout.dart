5c84a191d4dd78fcb3af1e676200a88240a1b21c
lagg99 <pro.lagg99@gmail.com>
2024-10-06 16:10:15 +0100

    create [all expenses] widget part one

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/all_expenses.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
@@ -10,6 +11,15 @@ class DashBoardDesktopLayout extends StatelessWidget {
       children: [
         Expanded(
           child: CustomDrawer(),
+        ),
+        SizedBox(width: 32),
+        Expanded(
+          flex: 2,
+          child: Column(
+            children: [
+              AllExpenses(),
+            ],
+          ),
         ),
       ],
     );
