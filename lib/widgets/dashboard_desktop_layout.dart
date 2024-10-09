d7a243b2d556ad9eaf576735e117d70895210336
lagg99 <pro.lagg99@gmail.com>
2024-10-09 16:09:45 +0100

    dashboard desktop layout done

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_widget.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/widgets/income_section.dart';
 import 'package:responsive_dash_board/widgets/my_card_and_income_section.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
@@ -18,9 +17,7 @@ class DashBoardDesktopLayout extends StatelessWidget {
           child: AllExpensesAndQuickInvoiceSection(),
         ),
         SizedBox(width: 24),
-        // Expanded(child: MyCardAndTransactionSection()),
-        // Expanded(child: MyCardAndIncomeSection()),
-        Expanded(child: IncomeSection())
+        Expanded(child: MyCardAndIncomeSection()),
       ],
     );
   }
--- lib/widgets/my_card_and_income_section.dart
+++ lib/widgets/my_card_and_income_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/income_section.dart';
+import 'package:responsive_dash_board/widgets/my_card_and_transaction_section.dart';
 
 class MyCardAndIncomeSection extends StatelessWidget {
   const MyCardAndIncomeSection({
@@ -10,10 +11,10 @@ class MyCardAndIncomeSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        // SizedBox(height: 40),
-        // MyCardAndTransactionSection(),
-        // SizedBox(height: 24),
-        IncomeSection(),
+        SizedBox(height: 40),
+        MyCardAndTransactionSection(),
+        SizedBox(height: 24),
+        Expanded(child: IncomeSection()),
       ],
     );
   }
